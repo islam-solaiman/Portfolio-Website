54c925270e75b8da9d79664394be23cd33463c3c
Islam Solaiman <islam.solaiman92@gmail.com>
2024-05-12 13:45:15 +0400

    Initial Commit - Website Components

---
+++ src/App.js
@@ -0,0 +1,19 @@
+
+import Hero from "./componnets/2-hero/Hero";
+import Header from "./componnets/1-header/Header";
+import Main from "./componnets/3-main/Main";
+import Contact from "./componnets/4-contact/Contact";
+import Footer from "./componnets/5-footer/Footer";
+
+
+function App() {
+  return <div>
+    <Header />
+    <Hero />
+    <Main />
+    <Contact />
+    <Footer />
+  </div>
+}
+
+export default App;
