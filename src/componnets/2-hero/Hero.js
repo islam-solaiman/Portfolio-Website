466df47aa48d725c1293139d6704fc3f9383254a
Islam Solaiman <islam.solaiman92@gmail.com>
2024-05-24 17:47:42 +0400

    Hero Section  styling Update

--- src/componnets/2-hero/Hero.js
+++ src/componnets/2-hero/Hero.js
@@ -4,17 +4,18 @@ import  './hero.css'
 export default function Hero() {
   return (
     <section className='hero flex'>
-      <div className='left-section border'>
-        <div className='parent-avatar'>
+      <div className='left-section '>
+        <div className='parent-avatar flex'>
           <img src="./me.png" className="avatar" alt="me"/>
-          <span className='icon-verified'></span>
+          <div className='icon-verified'></div>
         </div>
-        <h3>Islam Solaiman, Software Enginer</h3>
-        <p>Diploma In Software Enigineering from Alx Afric</p>
-        <div>
-          <span className='icon-linkedin-square'></span>
-          <span className='icon-github'></span>
-          <span className='icon-instagram'></span>
+        <h3 className='title'>Islam Solaiman,</h3>
+        <h3 className='title'>Software Enginer</h3>
+        <p className='sub-title'>Diploma In Software Enigineering from Alx Afric</p>
+        <div className='all-icons flex'>
+          <div className='icon icon-linkedin-square'></div>
+          <div className='icon icon-github'></div>
+          <div className='icon icon-instagram'></div>
         </div>
       </div>
       <div className='right-section border'>
