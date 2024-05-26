da4c35653b7701480abd36943eea5fb65b94f206
Islam Solaiman <islam.solaiman92@gmail.com>
2024-05-26 16:08:00 +0400

    Main Section

--- src/componnets/3-main/Main.js
+++ src/componnets/3-main/Main.js
@@ -3,8 +3,37 @@ import  './main.css'
 
 export default function Main() {
   return (
-    <div>
-      Main
-    </div>
+    <main className='flex'>
+      <section className='flex left-section'>
+        <button className='active'>All Projects</button>
+        <button>HTML & CSS</button>
+        <button>JavaScript</button>
+        <button>React & MUI</button>
+        <button>NodeJs & Express</button>
+      </section>
+
+      <section className='border right-section'>
+        <article className='card'>
+          <img src='' alt=''/>
+          <div style={{width:"266px"}} className='border box'>
+            <h1 classname='title'>
+              My Gallery
+            </h1>
+            <p className='sub-title'>
+            Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard 
+            </p>
+            <div className='flex'>
+              <div></div>
+              <div className='icon-link'></div>
+              <div className='icon-github'></div>
+              <a href="#">
+                More
+                <span className='icon-arrow_forward'></span>
+              </a>
+            </div>
+          </div>
+        </article>
+      </section>
+    </main>
   )
 }
