c1f254d95cb616e251a1f6046389db450db36971
Islam Solaiman <islam.solaiman92@gmail.com>
2024-05-30 18:02:07 +0400

    Main Section styling Update

--- src/componnets/3-main/Main.js
+++ src/componnets/3-main/Main.js
@@ -13,27 +13,32 @@ export default function Main() {
       </section>
 
       <section className='right-section'>
-        <article className='card'>
-          <img src='./1.jpg' alt=''/>
-          <div style={{width:"266px"}} className='box'>
-            <h1 classname='title'>
-              My Gallery
-            </h1>
-            <p className='sub-title'>
-            Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard 
-            </p>
-            <div className='flex icons'>
-              <div style={{gap: "11px"}} className='flex'>
-                <div className='icon-link'></div>
-                <div className='icon-github'></div>
-              </div>
-              <a className='link flex' href="#" >
-                More
-                <span style={{alignSelf: "end"}} className='icon-arrow_forward'></span>
-              </a>
-            </div>
-          </div>
-        </article>
+
+      {["a", "b", "c"].map((item) => {
+  return (
+    <article className='card'>
+    <img src='./1.jpg' alt=''/>
+    <div style={{width:"266px"}} className='box'>
+      <h1 classname='title'>
+        My Gallery
+      </h1>
+      <p className='sub-title'>
+      Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard 
+      </p>
+      <div className='flex icons'>
+        <div style={{gap: "11px"}} className='flex'>
+          <div className='icon-link'></div>
+          <div className='icon-github'></div>
+        </div>
+        <a className='link flex' href="#" >
+          More
+          <span style={{alignSelf: "end"}} className='icon-arrow_forward'></span>
+        </a>
+      </div>
+    </div>
+  </article> 
+  )
+})}
       </section>
     </main>
   )
