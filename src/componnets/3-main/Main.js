f2f71da0a1194e73dca4d53f7dea5878a76609af
Islam Solaiman <islam.solaiman92@gmail.com>
2024-06-18 18:58:03 +0400

    Main Section Logic Update

--- src/componnets/3-main/Main.js
+++ src/componnets/3-main/Main.js
@@ -1,31 +1,68 @@
 import React, { useState } from 'react'
 import  './main.css'
 
+
+const myProjects = [{
+  title: "first project",
+  category: ["HTML & CSS"],
+  imgPath: "./1.jpg", 
+  summery: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard ",
+  },
+  {
+    title: "Second project",
+    category: ["HTML & CSS", "javaScript"],
+    imgPath: "./1.jpg", 
+    summery: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard ",
+  },
+  {
+    title: "Third project",
+    category: ["javaScript"],
+    imgPath: "./1.jpg", 
+    summery: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard ",
+  },
+];
+
 export default function Main() {
-  const  [isActive, setIsActive] = useState("all");
+  const [isActive, setIsActive] = useState("all");
+  const [arr, setArr] = useState(myProjects);
 
   return (
     <main className='flex'>
       <section className='flex left-section'>
         <button onClick={()=>setIsActive("all")} className={isActive === "all"? "active" : ""}>All Projects</button>
-        <button onClick={()=>setIsActive("option1")} className={isActive === "option1" ? "active" : ""}>HTML & CSS</button>
-        <button onClick={()=>setIsActive("option2")} className={isActive === "option2" ? "active" : ""}>JavaScript</button>
-        <button onClick={()=>setIsActive("option3")} className={isActive === "option3" ? "active" : ""}>React & MUI</button>
-        <button onClick={()=>setIsActive("option4")} className={isActive === "option4" ? "active" : ""}>NodeJs & Express</button>
+        <button onClick={()=>{setIsActive("option1"); 
+          const newArr = myProjects.filter((item) => {
+            const categ_item = item.category.filter((i) => {
+            return i === "HTML & CSS"
+          })
+            return categ_item[0] === "HTML & CSS"
+          })
+          setArr(newArr)}} className={isActive === "option1" ? "active" : ""}>HTML & CSS</button>
+        <button onClick={()=>{setIsActive("option2"); 
+          const newArr = myProjects.filter((item) => {
+            const categ_item = item.category.filter((i) => {
+               
+              return i === "javaScript"}
+          )
+            return categ_item[0] === "javaScript"
+          })
+          setArr(newArr)}} className={isActive === "option2" ? "active" : ""}>JavaScript</button>
+        <button onClick={()=>{setIsActive("option3"); setArr()}} className={isActive === "option3" ? "active" : ""}>React & MUI</button>
+        <button onClick={()=>{setIsActive("option4"); setArr()}} className={isActive === "option4" ? "active" : ""}>NodeJs & Express</button>
       </section>
 
       <section className='flex right-section'>
 
-      {["a", "b", "c", 1, 2].map((item) => {
+      {arr.map((item) => {
   return (
     <article className='card'>
-    <img src='./1.jpg' alt=''/>
+    <img src={item.imgPath} alt={item.title}/>
     <div style={{width:"266px"}} className='box'>
       <h1 classname='title'>
-        My Gallery
+        {item.title}
       </h1>
       <p className='sub-title'>
-      Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard 
+        {item.summery}
       </p>
       <div className='flex icons'>
         <div style={{gap: "11px"}} className='flex'>
