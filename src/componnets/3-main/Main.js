c642b55f23000241aa2dc3e0ed87ac22337d180b
Islam Solaiman <islam.solaiman92@gmail.com>
2024-06-27 17:58:45 +0400

    Main Section Logic Update

--- src/componnets/3-main/Main.js
+++ src/componnets/3-main/Main.js
@@ -1,39 +1,6 @@
 import React, { useState } from 'react'
 import  './main.css'
-
-
-const myProjects = [{
-  title: "HTML Project",
-  category: ["HTML & CSS"],
-  imgPath: "./1.jpg", 
-  summery: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard ",
-  },
-  {
-    title: "HTML And Javascript project",
-    category: ["HTML & CSS", "JavaScript"],
-    imgPath: "./1.jpg", 
-    summery: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard ",
-  },
-  {
-    title: "JavaScript project",
-    category: ["JavaScript"],
-    imgPath: "./1.jpg", 
-    summery: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard ",
-  },
-  {
-  title: "React project",
-  category: ["React"],
-  imgPath: "./1.jpg", 
-  summery: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard ",
-  },
-  {
-    title: "NodeJs project",
-    category: ["NodeJs & Express"],
-    imgPath: "./1.jpg", 
-    summery: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard ",
-    },
-];
-
+import { myProjects } from './myProjects';
 
 export default function Main() {
   const [isActive, setIsActive] = useState("all");
---
+++ src/componnets/3-main/myProjects.js
@@ -0,0 +1,31 @@
+export const myProjects = [{
+    title: "HTML Project",
+    category: ["HTML & CSS"],
+    imgPath: "./1.jpg", 
+    summery: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard ",
+    },
+    {
+      title: "HTML And Javascript project",
+      category: ["HTML & CSS", "JavaScript"],
+      imgPath: "./1.jpg", 
+      summery: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard ",
+    },
+    {
+      title: "JavaScript project",
+      category: ["JavaScript"],
+      imgPath: "./1.jpg", 
+      summery: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard ",
+    },
+    {
+    title: "React project",
+    category: ["React"],
+    imgPath: "./1.jpg", 
+    summery: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard ",
+    },
+    {
+      title: "NodeJs project",
+      category: ["NodeJs & Express"],
+      imgPath: "./1.jpg", 
+      summery: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard ",
+      },
+  ];
