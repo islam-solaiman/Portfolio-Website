2b5b59d75cc227ba728c13cfc62a917872d45a80
Islam Solaiman <islam.solaiman92@gmail.com>
2024-07-12 13:02:47 +0400

    Site Animation

--- src/componnets/2-hero/Hero.js
+++ src/componnets/2-hero/Hero.js
@@ -1,30 +1,56 @@
-import React from 'react'
-import  './hero.css'
+import "./hero.css";
 import Lottie from "lottie-react";
 import developerAnimation from "../../animation/developer1.json";
+import { useRef } from "react";
+import { motion } from "framer-motion";
 
 export default function Hero() {
+  const lottieRef = useRef();
+
   return (
-    <section className='hero flex' >
-      <div className='left-section '>
-        <div className='parent-avatar flex'>
-          <img src="./me.png" className="avatar" alt="me"/>
-          <div className='icon-verified'></div>
+    <section className="hero flex">
+      <div className="left-section ">
+        <div className="parent-avatar flex">
+          <motion.img
+            initial={{ transform: "scale(0)" }}
+            animate={{ transform: "scale(1.1)" }}
+            transition={{ damping: 6, type: "spribg", stiffness: 100 }}
+            src="./me.png"
+            className="avatar"
+            alt="me"
+          />
+          <div className="icon-verified"></div>
         </div>
-        <h3 className='title'>Islam Solaiman,</h3>
-        <h3 className='title'>Software Enginer</h3>
-        <p className='sub-title'>I'm islam Solaiman, i have diploma In Software Enigineering from Alx Afric</p>
-        <div className='all-icons flex'>
-          <div className='icon icon-linkedin-square'></div>
-          <div className='icon icon-github'></div>
-          <div className='icon icon-instagram'></div>
+        <motion.h3
+          initial={{ opacity: 0 }}
+          animate={{ opacity: 1 }}
+          transition={{ duration: 2 }}
+          className="title"
+        >
+          Islam Solaiman, Software Engineer
+        </motion.h3>
+        <p className="sub-title">
+          I'm islam Solaiman, i have diploma In Software Enigineering from Alx
+          Afric
+        </p>
+        <div className="all-icons flex">
+          <div className="icon icon-linkedin-square"></div>
+          <div className="icon icon-github"></div>
+          <div className="icon icon-instagram"></div>
         </div>
       </div>
-      <div className='right-section animation'>
-            <Lottie 
-            animationData={developerAnimation}
-          />
+      <div className="right-section animation">
+        <Lottie
+          lottieRef={lottieRef}
+          className=""
+          onLoadedImages={() => {
+            // @ts-ignore
+            // https://lottiereact.com/
+            lottieRef.current.setSpeed(0.5);
+          }}
+          animationData={developerAnimation}
+        />
       </div>
     </section>
-  )
+  );
 }
--- src/componnets/3-main/Main.js
+++ src/componnets/3-main/Main.js
@@ -1,63 +1,95 @@
-import React, { useState } from 'react'
-import  './main.css'
-import { myProjects } from './myProjects';
+import React, { useState } from "react";
+import "./main.css";
+import { myProjects } from "./myProjects";
+import { AnimatePresence, motion } from "framer-motion";
 
 export default function Main() {
   const [isActive, setIsActive] = useState("all");
   const [arr, setArr] = useState(myProjects);
 
-  const  handleClick = (buttonCategory) => {
-    setIsActive(buttonCategory); 
-      const newArr = myProjects.filter((item) => {
-        const categ_item = item.category.filter((i) => {
-           
-          return i === buttonCategory}
-      )
-        return categ_item[0] === buttonCategory
-      })
-      setArr(newArr);
-    }
+  const handleClick = (buttonCategory) => {
+    setIsActive(buttonCategory);
+    const newArr = myProjects.filter((item) => {
+      const categ_item = item.category.filter((i) => {
+        return i === buttonCategory;
+      });
+      return categ_item[0] === buttonCategory;
+    });
+    setArr(newArr);
+  };
 
   return (
-    <main className='flex'>
-      <section className='flex left-section'>
-        <button onClick={()=>{setIsActive("all"); setArr(myProjects)}} className={isActive === "all"? "active" : ""}>All Projects</button>
-        <button onClick={()=> handleClick("HTML & CSS")} 
-          className={isActive === "HTML & CSS" ? "active" : ""}>HTML & CSS</button>
-        <button onClick={()=> handleClick("JavaScript")} 
-        className={isActive === "JavaScript" ? "JavaScript" : ""}>JavaScript</button>
-        <button onClick={()=> handleClick("React")} className={isActive === "React" ? "active" : ""}>React</button>
-        <button onClick={()=> handleClick("NodeJs & Express")} className={isActive === "NodeJs & Express" ? "active" : ""}>NodeJs & Express</button>
+    <main className="flex">
+      <section className="flex left-section">
+        <button
+          onClick={() => {
+            setIsActive("all");
+            setArr(myProjects);
+          }}
+          className={isActive === "all" ? "active" : ""}
+        >
+          All Projects
+        </button>
+        <button
+          onClick={() => handleClick("HTML & CSS")}
+          className={isActive === "HTML & CSS" ? "active" : ""}
+        >
+          HTML & CSS
+        </button>
+        <button
+          onClick={() => handleClick("JavaScript")}
+          className={isActive === "JavaScript" ? "active" : ""}
+        >
+          JavaScript
+        </button>
+        <button
+          onClick={() => handleClick("React")}
+          className={isActive === "React" ? "active" : ""}
+        >
+          React
+        </button>
+        <button
+          onClick={() => handleClick("NodeJs & Express")}
+          className={isActive === "NodeJs & Express" ? "active" : ""}
+        >
+          NodeJs & Express
+        </button>
       </section>
 
-      <section className='flex right-section'>
-
-      {arr.map((item) => {
-  return (
-    <article className='card'>
-    <img src={item.imgPath} alt={item.title}/>
-    <div style={{width:"266px"}} className='box'>
-      <h1 classname='title'>
-        {item.title}
-      </h1>
-      <p className='sub-title'>
-        {item.summery}
-      </p>
-      <div className='flex icons'>
-        <div style={{gap: "11px"}} className='flex'>
-          <div className='icon-link'></div>
-          <div className='icon-github'></div>
-        </div>
-        <a className='link flex' href="#" >
-          More
-          <span style={{alignSelf: "end"}} className='icon-arrow_forward'></span>
-        </a>
-      </div>
-    </div>
-  </article> 
-  )
-})}
+      <section className="flex right-section">
+        <AnimatePresence>
+          {arr.map((item) => {
+            return (
+              <motion.article
+              layout
+              initial={{ transform: "scale(0.4)" }}
+              animate={{ transform: "scale(1)" }}
+              transition={{ type: "spring", damping: 8, stiffness: 30 }}
+              key={item.imgPath}
+              className="card">
+                <img src={item.imgPath} alt={item.title} />
+                <div style={{ width: "266px" }} className="box">
+                  <h1 classname="title">{item.title}</h1>
+                  <p className="sub-title">{item.summery}</p>
+                  <div className="flex icons">
+                    <div style={{ gap: "11px" }} className="flex">
+                      <div className="icon-link"></div>
+                      <div className="icon-github"></div>
+                    </div>
+                    <a className="link flex" href="#">
+                      More
+                      <span
+                        style={{ alignSelf: "end" }}
+                        className="icon-arrow_forward"
+                      ></span>
+                    </a>
+                  </div>
+                </div>
+              </motion.article>
+            );
+          })}
+        </AnimatePresence>
       </section>
     </main>
-  )
+  );
 }
